ec925932c45c51635724398b3c37f7d6c1aa191f
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2025-02-26 23:39:10 -0500

    Create bit setting macro and register name table

--- src/disassembler.c
+++ src/disassembler.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 
+char reg_table[][2][3] = {
+    [0b000] = {"AL", "AX"},
+    [0b001] = {"CL", "CX"},
+    [0b010] = {"DL", "DX"},
+    [0b011] = {"BL", "BX"},
+    [0b100] = {"AH", "SP"},
+    [0b101] = {"CH", "BP"},
+    [0b110] = {"DH", "SI"},
+    [0b111] = {"BH", "DI"},
+};
+
+#define BIT(n) (1U << n)
+
 int main(int argc, char *argv[]) {
 
-    FILE *fs = fopen(argv[1], "rw+");
+    FILE *fs = fopen(argv[1], "w+");
 
     if (!fs) {
         printf("Failed to read file\n");
     }
 
-    char inst_buf[2];
+    u8 inst_buf[2];
 
     size_t bytes_read = fread(inst_buf, sizeof(inst_buf), 1, fs);
 
@@ -17,6 +30,8 @@ int main(int argc, char *argv[]) {
         printf("Failed to read bytes %d\n", bytes_read);
         feof(bytes_read);
     }
+
+    
     
 
     return 0;
